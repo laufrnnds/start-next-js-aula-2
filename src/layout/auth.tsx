e6f06a3a10cdddb2d2fcd5cdcf5f1d41017565d2
Laura Fernandes <lauraifernandes2@gmail.com>
2024-04-23 17:44:55 -0300

    first commit

---
+++ src/layout/auth.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/pages/components/navbar";
+
+export default function AuthLayout(page: any) {
+  return (
+    <>
+      <Navbar />
+      <div style={{ background: "#e71414e2", borderRadius: "20px" }}>
+        {page}
+      </div>
+    </>
+  );
+}
